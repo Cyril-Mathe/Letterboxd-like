b9fc968205f405a5fa518775d9f48ed6a6c2a00e
Zaco <cyrilmathe08@gmail.com>
2026-01-05 14:53:29 +0100

    :sparkles::white_check_mark: controllers et routes fontionnels pour réalisateur

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,7 @@
 // routes
 import express from "express";
 import { getActors, getActorsById, createActors, modifyActors, deleteActors } from "../controllers/Actors/actorsControllers.js";
+import { getDirectors, getDirectorsById, createDirectors, modifyDirectors, deleteDirectors } from "../controllers/Directors/directorsControllers.js";
 
 const router = express.Router()
 
@@ -10,5 +11,12 @@ router.get('/actors/:id', getActorsById);
 router.post('/actors', createActors);
 router.put('/actors/:id', modifyActors);
 router.delete('/actors/:id', deleteActors);
+
+// directors routes
+router.get('/directors', getDirectors);
+router.get('/directors/:id', getDirectorsById);
+router.post('/directors', createDirectors);
+router.put('/directors/:id', modifyDirectors);
+router.delete('/directors/:id', deleteDirectors);
 
 export default router;
