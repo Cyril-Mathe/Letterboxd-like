86dd81007fd42527b3b9a1b3376670448368ab63
Zaco <cyrilmathe08@gmail.com>
2026-02-19 16:52:55 +0100

    :art: ajout films d'horreur et de sf pour recommendations

--- frontend/src/recommendationsPage.jsx
+++ frontend/src/recommendationsPage.jsx
@@ -33,7 +33,7 @@ export default function RecommendationsPage() {
         const crime4 = await fetch(`https://www.omdbapi.com/?t=heat&apikey=${apiKey}`)
         const crime5 = await fetch(`https://www.omdbapi.com/?t=city%20of%20god&apikey=${apiKey}`)
         const crime6 = await fetch(`https://www.omdbapi.com/?t=training%20day&apikey=${apiKey}`)
-        const crime7 = await fetch(`https://www.omdbapi.com/?t=the%20departed&apikey=${apiKey}`)
+        const crime7 = await fetch(`https://www.omdbapi.com/?t=the%20irishman&apikey=${apiKey}`)
         const crime8 = await fetch(`https://www.omdbapi.com/?t=dog%20day%20afternoon&apikey=${apiKey}`)
         const data1c = await crime1.json()
         const data2c = await crime2.json()
@@ -48,7 +48,7 @@ export default function RecommendationsPage() {
         const action2 = await fetch(`https://www.omdbapi.com/?t=furiosa&apikey=${apiKey}`)
         const action3 = await fetch(`https://www.omdbapi.com/?t=seven%20samurai&apikey=${apiKey}`)
         const action4 = await fetch(`https://www.omdbapi.com/?t=terminator%20ii&apikey=${apiKey}`)
-        const action5 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
+        const action5 = await fetch(`https://www.omdbapi.com/?t=pacific%20rim&apikey=${apiKey}`)
         const action6 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
         const action7 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
         const action8 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
@@ -62,12 +62,12 @@ export default function RecommendationsPage() {
         const data8a = await action8.json()
         // fetch de westerns
         const western1 = await fetch(`https://www.omdbapi.com/?t=no%20country%20for%20old%20men&apikey=${apiKey}`)
-        const western2 = await fetch(`https://www.omdbapi.com/?t=the%20good%20the%bad%20and%20the%20ugly&apikey=${apiKey}`)
-        const western3 = await fetch(`https://www.omdbapi.com/?t=for%20a%20few%20dollars%20more&apikey=${apiKey}`)
-        const western4 = await fetch(`https://www.omdbapi.com/?t=django%20unchained&apikey=${apiKey}`)
-        const western5 = await fetch(`https://www.omdbapi.com/?t=a%20fistful%20of%20dollars&apikey=${apiKey}`)
-        const western6 = await fetch(`https://www.omdbapi.com/?t=the%20hateful%20eight&apikey=${apiKey}`)
-        const western7 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
+        const western2 = await fetch(`https://www.omdbapi.com/?t=the%20good%20the%20bad%20and%20the%20ugly&apikey=${apiKey}`)
+        const western3 = await fetch(`https://www.omdbapi.com/?t=once%20upon%20a%20time%20in%20the%20west&apikey=${apiKey}`)
+        const western4 = await fetch(`https://www.omdbapi.com/?t=for%20a%20few%20dollars%20more&apikey=${apiKey}`)
+        const western5 = await fetch(`https://www.omdbapi.com/?t=django%20unchained&apikey=${apiKey}`)
+        const western6 = await fetch(`https://www.omdbapi.com/?t=a%20fistful%20of%20dollars&apikey=${apiKey}`)
+        const western7 = await fetch(`https://www.omdbapi.com/?t=the%20hateful%20eight&apikey=${apiKey}`)
         const western8 = await fetch(`https://www.omdbapi.com/?t=&apikey=${apiKey}`)
         const data1w = await western1.json()
         const data2w = await western2.json()
@@ -77,8 +77,42 @@ export default function RecommendationsPage() {
         const data6w = await western6.json()
         const data7w = await western7.json()
         const data8w = await western8.json()
+        // fetch de films d'horreur
+        const horreur1 = await fetch(`https://www.omdbapi.com/?t=the%20thing&apikey=${apiKey}`)
+        const horreur2 = await fetch(`https://www.omdbapi.com/?t=psycho&apikey=${apiKey}`)
+        const horreur3 = await fetch(`https://www.omdbapi.com/?t=the%20shining&apikey=${apiKey}`)
+        const horreur4 = await fetch(`https://www.omdbapi.com/?t=alien&apikey=${apiKey}`)
+        const horreur5 = await fetch(`https://www.omdbapi.com/?t=the%20substance&apikey=${apiKey}`)
+        const horreur6 = await fetch(`https://www.omdbapi.com/?t=the%20others&apikey=${apiKey}`)
+        const horreur7 = await fetch(`https://www.omdbapi.com/?t=requiem%20for%20a%20dream&apikey=${apiKey}`)
+        const horreur8 = await fetch(`https://www.omdbapi.com/?t=sinners&apikey=${apiKey}`)
+        const data1h = await horreur1.json()
+        const data2h = await horreur2.json()
+        const data3h = await horreur3.json()
+        const data4h = await horreur4.json()
+        const data5h = await horreur5.json()
+        const data6h = await horreur6.json()
+        const data7h = await horreur7.json()
+        const data8h = await horreur8.json()
+        // fetch de films de science-fiction
+        const sf1 = await fetch(`https://www.omdbapi.com/?t=2001%20a%20space%20odyssey&apikey=${apiKey}`)
+        const sf2 = await fetch(`https://www.omdbapi.com/?t=stalker&apikey=${apiKey}`)
+        const sf3 = await fetch(`https://www.omdbapi.com/?t=akira&apikey=${apiKey}`)
+        const sf4 = await fetch(`https://www.omdbapi.com/?t=blade%20runner%202049&apikey=${apiKey}`)
+        const sf5 = await fetch(`https://www.omdbapi.com/?t=aliens&apikey=${apiKey}`)
+        const sf6 = await fetch(`https://www.omdbapi.com/?t=12%20monkeys&apikey=${apiKey}`)
+        const sf7 = await fetch(`https://www.omdbapi.com/?t=arrival&apikey=${apiKey}`)
+        const sf8 = await fetch(`https://www.omdbapi.com/?t=a%20clockwork%20orange&apikey=${apiKey}`)
+        const data1sf = await sf1.json()
+        const data2sf = await sf2.json()
+        const data3sf = await sf3.json()
+        const data4sf = await sf4.json()
+        const data5sf = await sf5.json()
+        const data6sf = await sf6.json()
+        const data7sf = await sf7.json()
+        const data8sf = await sf8.json()
         return {
-          data1, data2, data3, data4, data5, data6, data7, data8, data1c, data2c, data3c, data4c, data5c, data6c, data7c, data8c, data1a, data2a, data3a, data4a, data5a, data6a, data7a, data8a, data1w, data2w, data3w, data4w, data5w, data6w, data7w, data8w
+          data1, data2, data3, data4, data5, data6, data7, data8, data1c, data2c, data3c, data4c, data5c, data6c, data7c, data8c, data1a, data2a, data3a, data4a, data5a, data6a, data7a, data8a, data1w, data2w, data3w, data4w, data5w, data6w, data7w, data8w, data1h, data2h, data3h, data4h, data5h, data6h, data7h, data8h, data1sf, data2sf, data3sf, data4sf, data5sf ,data6sf, data7sf, data8sf
         }
       },
       staleTime: 1000 * 30,
@@ -208,8 +242,58 @@ export default function RecommendationsPage() {
             </div>
           )}
 
-          {categories === "horreur" && <div>BB</div>}
-          {categories === "science-fiction" && <div>CC</div>}
+          {categories === "horreur" && (
+            <div className='h-screen flex justify-center'>
+              <div className='w-[700px] flex flex-wrap'>
+                {data && ([data.data1h, data.data2h, data.data3h, data.data4h, data.data5h, data.data6h, data.data7h, data.data8h].filter(Boolean)).map((movie) => (
+                  <div className='w-[175px]' key={movie.imdbID}>
+                    {movie.Poster && movie.Poster !== 'N/A' && (
+                      <div
+                        onClick={() => navigate({ to: `/movies/${movie.imdbID}` })}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        <img 
+                          src={movie.Poster} 
+                          alt={movie.Title} 
+                          width={150}
+                          style={{ display: 'block' }}
+                        />
+                      </div>
+                    )}
+                    <p>{movie.Title}</p>
+                    {movie.imdbRating && <p>Note IMDb : {movie.imdbRating}/10</p>}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
+          {categories === "science-fiction" && (
+            <div className='h-screen flex justify-center'>
+              <div className='w-[700px] flex flex-wrap'>
+                {data && ([data.data1sf, data.data2sf, data.data3sf, data.data4sf, data.data5sf, data.data6sf, data.data7sf, data.data8sf].filter(Boolean)).map((movie) => (
+                  <div className='w-[175px]' key={movie.imdbID}>
+                    {movie.Poster && movie.Poster !== 'N/A' && (
+                      <div
+                        onClick={() => navigate({ to: `/movies/${movie.imdbID}` })}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        <img 
+                          src={movie.Poster} 
+                          alt={movie.Title} 
+                          width={150}
+                          style={{ display: 'block' }}
+                        />
+                      </div>
+                    )}
+                    <p>{movie.Title}</p>
+                    {movie.imdbRating && <p>Note IMDb : {movie.imdbRating}/10</p>}
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+
           {categories === "animation" && <div>DD</div>}
           {categories === "thriller" && <div>AA</div>}
           {categories === "comédie" && <div>EE</div>}
